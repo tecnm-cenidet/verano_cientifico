ce761db469e4667a064da94290a88bc54e07b80f
Viter <vitervolopezcaballero@gmail.com>
2021-12-31 11:20:52 -0600

    Se agrega seguridad en las variables de sesiones

--- Controllers/Tematics.php
+++ Controllers/Tematics.php
@@ -5,6 +5,7 @@ class Tematics extends Controllers
     {
         parent::__construct();
         session_start();
+        session_regenerate_id(true);
         if (empty($_SESSION['login'])) {
             header('Location:' . base_url() . '/login');
         }
